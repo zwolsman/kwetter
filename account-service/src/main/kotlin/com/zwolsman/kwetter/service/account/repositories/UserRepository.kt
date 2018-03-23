eaeda3199989746e73a07f91bf6a8490ab2d116a
Marvin <marvin.zwolsman@gmail.com>
2018-03-23 11:43:30 +0100

    Primary key of an user is a string not ObjectId

--- account-service/src/main/kotlin/com/zwolsman/kwetter/service/account/repositories/UserRepository.kt
+++ account-service/src/main/kotlin/com/zwolsman/kwetter/service/account/repositories/UserRepository.kt
@@ -4,6 +4,6 @@ import com.zwolsman.kwetter.dao.models.KwetterUser
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface UserRepository : MongoRepository<KwetterUser, ObjectId> {
+interface UserRepository : MongoRepository<KwetterUser, String> {
     fun findByUsername(username: String) : KwetterUser?
 }
