91b614e3081034536c3d97b92835157aa3399239
Marvin <marvin.zwolsman@gmail.com>
2018-03-24 14:57:04 +0100

    Basic showing of kweets

---
+++ kwetter-ios/kwetter-ios/TimelineTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  TimelineTableViewCell.swift
+//  kwetter-ios
+//
+//  Created by Marvin Zwolsman on 24/03/2018.
+//  Copyright © 2018 Marvin Zwolsman. All rights reserved.
+//
+
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var timeAgo: UILabel!
+    @IBOutlet weak var content: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func setup(kweet: Kweet) {
+        content.text = kweet.text
+        userName.text = "@" + kweet.user.username
+        timeAgo.text = "0s"
+    }
+
+}
--- kwetter-ios/kwetter-ios/controllers/TimelineViewController.swift
+++ kwetter-ios/kwetter-ios/controllers/TimelineViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+let cellIdentifier = "kweetCell"
 class TimelineController : UITableViewController {
     
     let filename = "mockup"
@@ -18,8 +19,8 @@ class TimelineController : UITableViewController {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
                 let jsonResult = try JSONDecoder().decode([Kweet].self, from: data)
                 kweets += jsonResult
+                tableView.reloadData()
                 print("Loaded kweets")
-                debugPrint(kweets)
             } catch {
                 // handle error
                 print("OH GOD")
@@ -27,7 +28,18 @@ class TimelineController : UITableViewController {
         }
     }
     
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return kweets.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! TimelineTableViewCell
+        cell.setup(kweet: kweets[indexPath.row])
+        return cell
     }
 }
