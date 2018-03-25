0a70db9aafc7ff2687383f6430b48481afd2ddbc
Marvin <marvin.zwolsman@gmail.com>
2018-03-25 15:01:28 +0200

    Resizing profile image and loading personal info

---
+++ kwetter-ios/kwetter-ios/controllers/ProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewController.swift
+//  kwetter-ios
+//
+//  Created by Marvin Zwolsman on 25/03/2018.
+//  Copyright © 2018 Marvin Zwolsman. All rights reserved.
+//
+
+import Foundation
---
+++ kwetter-ios/kwetter-ios/models/KweetUserResource.swift
@@ -0,0 +1,9 @@
+//
+//  KweetUserResource.swift
+//  kwetter-ios
+//
+//  Created by Marvin Zwolsman on 25/03/2018.
+//  Copyright © 2018 Marvin Zwolsman. All rights reserved.
+//
+
+import Foundation
